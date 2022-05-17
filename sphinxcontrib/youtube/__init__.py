81f4a9f334a2c94e39534a59f3ddf01a4bb5af70
Thomas Braun <thomas.braun@byte-physics.de>
2022-05-17 18:36:27 +0200

    setup: Allow parallel read/write for extension
    Untested.

--- sphinxcontrib/youtube/__init__.py
+++ sphinxcontrib/youtube/__init__.py
@@ -5,3 +5,5 @@ def setup(app):
     app.add_directive("youtube", youtube.YouTube)
     app.add_node(vimeo.vimeo, **vimeo._NODE_VISITORS)
     app.add_directive("vimeo", vimeo.Vimeo)
+
+    return {"version": __version__, "parallel_read_safe": True, "parallel_write_safe": True}
